35893b9ad3e5c2eb615dff11a8064d6d7e3ea152
mm68-github <vaanfilm68@yahoo.co.jp>
2025-01-07 15:36:19 +0900

    施設登録、予約機能

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,9 @@
 class Room < ApplicationRecord
+  belongs_to :user
+  has_many :reservations
+
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :address, presence: true
 end
